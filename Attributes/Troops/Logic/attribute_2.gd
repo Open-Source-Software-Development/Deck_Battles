c8ba2e2db9f000bc462cc46caab841696afe2f25
AnthonyBatt <abattag2@nd.edu>
2024-05-29 15:39:38 -0400

    Fix 2 and 3
    Had some errors in how attributes give building based bonuses check for those bonuses because cities aren't technically buildings

--- Attributes/Troops/Logic/attribute_2.gd
+++ Attributes/Troops/Logic/attribute_2.gd
@@ -16,19 +16,19 @@ func on_moved(from: Vector2i, to: Vector2i):
 	var b_fr = board.buildings[from.x][from.y]
 	
 	# Defendable is building attribute 0
-	if b_to is City or 0 in b_to.attr_ids:
-		if b_fr is City or 0 in b_fr.attr_ids:
+	if b_to != null and (b_to is City or 0 in b_to.attr_ids):
+		if b_fr != null and (b_fr is City or 0 in b_fr.attr_ids):
 			return
 		logger.debug('troop_attribute', '(FORTFY) Adding defense bonus of %d' % bonus)
 		parent.defense += bonus
 	else:
-		if b_fr is City or 0 in b_fr.attr_ids:
+		if b_fr != null and (b_fr is City or 0 in b_fr.attr_ids):
 			logger.debug('troop_attribute', '(FORTFY) Removing defense bonus')
 			parent.defense -= bonus
 
 	
 func reset():
 	var b = board.buildings[parent.pos.x][parent.pos.y]
-	if  b is City or 0 in b.attr_ids:
+	if  b != null and (b is City or 0 in b.attr_ids):
 		logger.debug('troop_attribute', '(FORTFY) Adding defense bonus of %d' % bonus)
 		parent.defense += bonus
--- Attributes/Troops/Logic/attribute_3.gd
+++ Attributes/Troops/Logic/attribute_3.gd
@@ -15,17 +15,18 @@ func on_moved(from: Vector2i, to: Vector2i):
 	var b_fr = board.buildings[from.x][from.y]
 	
 	# tall is building attribute id 6
-	if b_to != null and 6 in b_to.attr_ids:
-		if b_fr != null and 6 in b_to.attr_ids:
+	if b_to != null and !(b_to is City) and 6 in b_to.attr_ids:
+		if b_fr != null and !(b_fr is City) and 6 in b_fr.attr_ids:
 			return
 		logger.debug('troop_attribute', '(TALL) Adding range bonus of %d' % bonus)
 		parent.rng += bonus
 	else:
-		if b_fr != null and 6 in b_to.attr_ids:
+		if b_fr != null and !(b_fr is City) and 6 in b_fr.attr_ids:
 			logger.debug('troop_attribute', '(TALL) Removing range bonus')
 			parent.rng -= bonus
 
 func reset():
-	if 6 in board.buildings[parent.pos.x][parent.pos.y].attr_ids:
+	var b = board.buildings[parent.pos.x][parent.pos.y]
+	if b != null and !(b is City) and 6 in b.attr_ids:
 		logger.debug('troop_attribute', '(TALL) Adding range bonus of %d' % bonus)
 		parent.rng += bonus
